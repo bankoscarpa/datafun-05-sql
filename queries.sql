9a8b72d49994a326cefd7d90e97dbc7bc7960f00
bankoscarpa <bankoscarpa@gmail.com>
2025-09-23 09:09:27 -0700

    updated queries and added books database

--- queries.sql
+++ queries.sql
@@ -1,6 +1,6 @@
 -- Query to count the number of books written by each author
-SELECT a.name AS author_name, COUNT(b.book_id) AS book_count
+SELECT a.first || ' ' || a.last AS author_name, COUNT(b.book_id) AS book_count
 FROM authors a
 LEFT JOIN books b ON a.author_id = b.author_id
-GROUP BY a.name
+GROUP BY a.first, a.last
 ORDER BY book_count DESC;
